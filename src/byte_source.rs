7a6bbee6dec1aa97fb976c316ae12d71aa54859b
Julien Férard <julien_ferard@yahoo.fr>
2021-06-01 20:42:27 +0200

    Use Option in byte_source.

--- src/byte_source.rs
+++ src/byte_source.rs
@@ -23,7 +23,8 @@ use std::io::Read;
 
 pub struct ByteSource<R: Read> {
     source: R,
-    buffer: [u8; 32769],
+    unget_byte: Option<u8>,
+    buffer: [u8; 32768],
     i: usize,
     limit: usize,
 }
@@ -32,34 +33,39 @@ impl<R: Read> ByteSource<R> {
     pub fn new(source: R) -> Self {
         ByteSource {
             source,
-            buffer: [0u8; 32 * 1024 + 1],
-            i: 1,
-            limit: 1,
+            unget_byte: None,
+            buffer: [0u8; 32 * 1024],
+            i: 0,
+            limit: 0,
         }
     }
 
     pub(crate) fn get(&mut self) -> Option<u8> {
-        if self.i >= self.limit {
-            self.i = 1;
-            self.buffer[0] = self.buffer[self.limit-1];
-            loop {
-                match self.source.read(&mut self.buffer[1..]) {
-                    Ok(0) => { return None; }
-                    Ok(n) => {
-                        self.limit = n+1;
-                        break;
-                    }
-                    Err(_) => {} // retry
-                };
+        if let Some(b) = self.unget_byte {
+            self.unget_byte = None;
+            Some(b)
+        } else {
+            if self.i >= self.limit {
+                self.i = 0;
+                loop {
+                    match self.source.read(&mut self.buffer[..]) {
+                        Ok(0) => { return None; }
+                        Ok(n) => {
+                            self.limit = n;
+                            break;
+                        }
+                        Err(_) => {} // retry
+                    };
+                }
             }
+            let j = self.i;
+            self.i += 1;
+            Some(self.buffer[j])
         }
-        let j = self.i;
-        self.i += 1;
-        Some(self.buffer[j])
     }
 
     pub(crate) fn unget(&mut self) {
-        self.i -= 1;
+        self.unget_byte = Some(self.buffer[self.i-1]);
     }
 }
 
