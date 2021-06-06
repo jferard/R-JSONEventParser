c5ce3afdf87dfcd31ac01b933f1303552cbe87d6
Julien Férard <julien_ferard@yahoo.fr>
2021-06-06 18:11:03 +0200

    Add tests.

--- tests/json2xml_test.rs
+++ tests/json2xml_test.rs
@@ -20,7 +20,7 @@
  */
 
 use std::{fs, io};
-use std::io::{Read, Write, ErrorKind};
+use std::io::{Write, ErrorKind};
 
 use r_json_event_parser::byte_source::ByteSource;
 use r_json_event_parser::json2xml::JSON2XMLConsumer;
@@ -30,35 +30,152 @@ use r_json_event_parser::json_parser::JSONParser;
 fn lex_example1() {
     let path = "tests/files/example1.json";
     let expected = fs::read_to_string("tests/files/example1.xml").unwrap();
-    test_file(path, expected.as_str());
+    let expected_argument = expected.as_str();
+    let f = fs::File::open(path).expect("no file found");
+    let read = f;
+    let expected = expected_argument;
+    let byte_source = ByteSource::new(read);
+    let mut buf = [0u8; 1024*1024];
+    let mut destination = BufWrite::new(&mut buf);
+    let mut consumer = JSON2XMLConsumer::new_formatted_and_typed(&mut destination);
+    let mut parser = JSONParser::new(byte_source, false);
+    let _ = parser.parse(&mut consumer);
+    assert_eq!(expected, destination.to_str());
+}
+
+#[test]
+fn lex_example1_no_type() {
+    let path = "tests/files/example1.json";
+    let f = fs::File::open(path).expect("no file found");
+    let byte_source = ByteSource::new(f);
+    let mut buf = [0u8; 1024*1024];
+    let mut destination = BufWrite::new(&mut buf);
+    let mut consumer = JSON2XMLConsumer::new_formatted(&mut destination);
+    let mut parser = JSONParser::new(byte_source, false);
+    let _ = parser.parse(&mut consumer);
+    assert_eq!(r#"<?xml version="1.0" encoding="utf-8"?>
+<root>
+    <glossary>
+        <title>example glossary</title>
+        <GlossDiv>
+            <title>S</title>
+            <GlossList>
+                <GlossEntry>
+                    <ID>SGML</ID>
+                    <SortAs>SGML</SortAs>
+                    <GlossTerm>Standard Generalized Markup Language</GlossTerm>
+                    <Acronym>SGML</Acronym>
+                    <Abbrev>ISO 8879:1986</Abbrev>
+                    <GlossDef>
+                        <para>A meta-markup language, used to create markup languages such as DocBook.</para>
+                        <GlossSeeAlso>
+                            <li>GML</li>
+                            <li>XML</li>
+                        </GlossSeeAlso>
+                    </GlossDef>
+                    <GlossSee>markup</GlossSee>
+                </GlossEntry>
+            </GlossList>
+        </GlossDiv>
+    </glossary>
+</root>
+"#, destination.to_str());
+}
+
+#[test]
+fn lex_example1_no_format() {
+    let path = "tests/files/example1.json";
+    let f = fs::File::open(path).expect("no file found");
+    let byte_source = ByteSource::new(f);
+    let mut buf = [0u8; 1024*1024];
+    let mut destination = BufWrite::new(&mut buf);
+    let mut consumer = JSON2XMLConsumer::new_typed(&mut destination);
+    let mut parser = JSONParser::new(byte_source, false);
+    let _ = parser.parse(&mut consumer);
+    assert_eq!(r#"<?xml version="1.0" encoding="utf-8"?>
+<root><glossary><title type="string">example glossary</title><GlossDiv><title type="string">S</title><GlossList><GlossEntry><ID type="string">SGML</ID><SortAs type="string">SGML</SortAs><GlossTerm type="string">Standard Generalized Markup Language</GlossTerm><Acronym type="string">SGML</Acronym><Abbrev type="string">ISO 8879:1986</Abbrev><GlossDef><para type="string">A meta-markup language, used to create markup languages such as DocBook.</para><GlossSeeAlso><li type="string">GML</li><li type="string">XML</li></GlossSeeAlso></GlossDef><GlossSee type="string">markup</GlossSee></GlossEntry></GlossList></GlossDiv></glossary></root>"#, destination.to_str());
+}
+
+#[test]
+fn lex_example1_no_format_no_type() {
+    let path = "tests/files/example1.json";
+    let f = fs::File::open(path).expect("no file found");
+    let byte_source = ByteSource::new(f);
+    let mut buf = [0u8; 1024*1024];
+    let mut destination = BufWrite::new(&mut buf);
+    let mut consumer = JSON2XMLConsumer::new_raw(&mut destination);
+    let mut parser = JSONParser::new(byte_source, false);
+    let _ = parser.parse(&mut consumer);
+    assert_eq!(r#"<?xml version="1.0" encoding="utf-8"?>
+<root><glossary><title>example glossary</title><GlossDiv><title>S</title><GlossList><GlossEntry><ID>SGML</ID><SortAs>SGML</SortAs><GlossTerm>Standard Generalized Markup Language</GlossTerm><Acronym>SGML</Acronym><Abbrev>ISO 8879:1986</Abbrev><GlossDef><para>A meta-markup language, used to create markup languages such as DocBook.</para><GlossSeeAlso><li>GML</li><li>XML</li></GlossSeeAlso></GlossDef><GlossSee>markup</GlossSee></GlossEntry></GlossList></GlossDiv></glossary></root>"#, destination.to_str());
 }
 
 #[test]
 fn lex_example2() {
     let path = "tests/files/example2.json";
     let expected = fs::read_to_string("tests/files/example2.xml").unwrap();
-    test_file(path, expected.as_str());
+    let expected_argument = expected.as_str();
+    let f = fs::File::open(path).expect("no file found");
+    let read = f;
+    let expected = expected_argument;
+    let byte_source = ByteSource::new(read);
+    let mut buf = [0u8; 1024*1024];
+    let mut destination = BufWrite::new(&mut buf);
+    let mut consumer = JSON2XMLConsumer::new_formatted_and_typed(&mut destination);
+    let mut parser = JSONParser::new(byte_source, false);
+    let _ = parser.parse(&mut consumer);
+    assert_eq!(expected, destination.to_str());
 }
 
 #[test]
 fn lex_example3() {
     let path = "tests/files/example3.json";
     let expected = fs::read_to_string("tests/files/example3.xml").unwrap();
-    test_file(path, expected.as_str());
+    let expected_argument = expected.as_str();
+    let f = fs::File::open(path).expect("no file found");
+    let read = f;
+    let expected = expected_argument;
+    let byte_source = ByteSource::new(read);
+    let mut buf = [0u8; 1024*1024];
+    let mut destination = BufWrite::new(&mut buf);
+    let mut consumer = JSON2XMLConsumer::new_formatted_and_typed(&mut destination);
+    let mut parser = JSONParser::new(byte_source, false);
+    let _ = parser.parse(&mut consumer);
+    assert_eq!(expected, destination.to_str());
 }
 
 #[test]
 fn lex_example4() {
     let path = "tests/files/example4.json";
     let expected = fs::read_to_string("tests/files/example4.xml").unwrap();
-    test_file(path, expected.as_str());
+    let expected_argument = expected.as_str();
+    let f = fs::File::open(path).expect("no file found");
+    let read = f;
+    let expected = expected_argument;
+    let byte_source = ByteSource::new(read);
+    let mut buf = [0u8; 1024*1024];
+    let mut destination = BufWrite::new(&mut buf);
+    let mut consumer = JSON2XMLConsumer::new_formatted_and_typed(&mut destination);
+    let mut parser = JSONParser::new(byte_source, false);
+    let _ = parser.parse(&mut consumer);
+    assert_eq!(expected, destination.to_str());
 }
 
 #[test]
 fn lex_example5() {
     let path = "tests/files/example5.json";
     let expected = fs::read_to_string("tests/files/example5.xml").unwrap();
-    test_file(path, expected.as_str());
+    let expected_argument = expected.as_str();
+    let f = fs::File::open(path).expect("no file found");
+    let read = f;
+    let expected = expected_argument;
+    let byte_source = ByteSource::new(read);
+    let mut buf = [0u8; 1024*1024];
+    let mut destination = BufWrite::new(&mut buf);
+    let mut consumer = JSON2XMLConsumer::new_formatted_and_typed(&mut destination);
+    let mut parser = JSONParser::new(byte_source, false);
+    let _ = parser.parse(&mut consumer);
+    assert_eq!(expected, destination.to_str());
 }
 
 struct BufWrite<'a> {
@@ -94,17 +211,3 @@ impl <'a> Write for &mut BufWrite<'a> {
     }
 }
 
-fn test_file(path: &str, expected: &str) {
-    let f = fs::File::open(path).expect("no file found");
-    test_read(f, expected);
-}
-
-fn test_read<R: Read>(read: R, expected: &str) {
-    let byte_source = ByteSource::new(read);
-    let mut buf = [0u8; 1024*1024];
-    let mut destination = BufWrite::new(&mut buf);
-    let mut consumer = JSON2XMLConsumer::new_formatted_and_typed(&mut destination);
-    let mut parser = JSONParser::new(byte_source, false);
-    let _ = parser.parse(&mut consumer);
-    assert_eq!(expected, destination.to_str());
-}
